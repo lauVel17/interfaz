edf06d82067957ee6e80c95f46628f1d0131ed1c
xeleneaperez <xperez7@estudiantes.areandina.edu.co>
2025-09-29 23:28:45 -0500

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 import { NgxSpinnerModule } from 'ngx-spinner';
-import { SwPush } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-root',
   standalone: true,
